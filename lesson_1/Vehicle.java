65e3b46263001e1121cfc2a7d40dd5cc5440bc17
vanarothorn <vanarothorn01@gmail.com>
2024-01-09 17:22:57 +0000

    Initial commit

---
+++ lesson_1/Vehicle.java
@@ -0,0 +1,13 @@
+class Vehicle {
+  public void startEngine(){
+    System.out.println("Starting Engine");
+  }
+
+  public void stopEngine(){
+    System.out.println("Stoping the engine");
+  }
+
+  public void accelerate(){
+    System.out.println("Accelerating...");
+  }
+}
